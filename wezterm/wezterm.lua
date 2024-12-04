7a41cd7dc1b7cf7194ffdb1a6a416c862c43973b
Kchun510 <kylechun1@gmail.com>
2024-12-04 11:27:59 -0800

    Adding, open terminal in fullscreen at launch

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,8 +4,13 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+local mux = wezterm.mux
 
+wezterm.on("gui-startup", function()
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():toggle_fullscreen()
+end)
+-- This is where you actually apply your config choices
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 18.0
 -- For example, changing the color scheme:
